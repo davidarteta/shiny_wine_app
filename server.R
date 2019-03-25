edcfb27b7b3a238c4f1663eb3b7dec5c618c5433
davidarteta <david_arteta@hotmail.com>
2019-03-25 10:44:49 +0100

    Added reactive functions

--- server.R
+++ server.R
@@ -10,20 +10,30 @@ library(ggplot2)
 
 server <- function(input, output) {
   
+  myvariable <- reactive({
+    input$variable
+  })
+  mygraph <- reactive({
+    input$graph
+  })
+  mycolor <- reactive({
+    input$color
+  })
+  
   output$mygraph <- renderPlot({
-    myvariable <- input$variable
-    mygraph <- input$graph
-    mycolor <- input$color
-    if (mycolor == "Grifols") {fill = "#003896"} else {fill = mycolor}
+    #myvariable <- input$variable
+    #mygraph <- input$graph
+    #mycolor <- input$color
+    if (mycolor() == "Grifols") {fill = "#003896"} else {fill = mycolor()}
     # create plot
-    if (input$graph == "Histogram"){
-      ggplot(wine, aes_string(myvariable)) +
+    if (mygraph() == "Histogram"){
+      ggplot(wine, aes_string(myvariable())) +
       geom_histogram(fill = fill, 
       colour = "grey30") + 
       theme_gray() 
       
-    } else if (input$graph == "Boxplot"){
-      ggplot(wine, aes_string(y=myvariable)) +
+    } else if (mygraph() == "Boxplot"){
+      ggplot(wine, aes_string(y=myvariable())) +
         geom_boxplot(width = 0.3, fill = fill, colour = "grey30", outlier.colour = "red", outlier.size = 3) + 
         coord_flip() +
         scale_x_continuous(limits=c(-0.7, 0.7)) +
@@ -31,7 +41,7 @@ server <- function(input, output) {
         theme(axis.title.y = element_blank(), axis.text.y = element_blank(),axis.ticks.y = element_blank())
     
     } else {#Stripchart
-      ggplot(wine, aes_string(x=myvariable, y = c(0))) +
+      ggplot(wine, aes_string(x=myvariable(), y = c(0))) +
         geom_point(colour = fill, size = 3) +
         theme(axis.title.y = element_blank(), axis.text.y = element_blank(),axis.ticks.y = element_blank())
     
